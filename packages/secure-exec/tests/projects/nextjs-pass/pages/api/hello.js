9861f1cac5de86de11902231276c412cf47bf0c6
Nathan Flurry <git@nathanflurry.com>
2026-03-18 15:49:52 -0700

    feat: US-161 - Add Next.js project-matrix fixture

---
+++ packages/secure-exec/tests/projects/nextjs-pass/pages/api/hello.js
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+
+exports.default = function handler(req, res) {
+	res.status(200).json({ message: "hello", method: req.method });
+};
